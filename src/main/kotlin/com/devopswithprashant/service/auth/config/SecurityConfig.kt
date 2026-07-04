735a3ddaa528924488df04d41fa789db550152a0
devopswithprashant <devopswithprashant@gmail.com>
2026-07-04 18:54:21 +0530

    feat: foundation of auth service

--- src/main/kotlin/com/devopswithprashant/service/auth/config/SecurityConfig.kt
+++ src/main/kotlin/com/devopswithprashant/service/auth/config/SecurityConfig.kt
@@ -2,15 +2,41 @@ package com.devopswithprashant.service.auth.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 class SecurityConfig {
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
+    fun securityFilterChain(
+        http: HttpSecurity
+    ): SecurityFilterChain {
 
-        return BCryptPasswordEncoder()
+        http
+            .csrf { it.disable() }
+
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+
+            .authorizeHttpRequests {
+
+                it.requestMatchers(
+                    HttpMethod.POST,
+                    "/api/v1/auth/register",
+                    "/api/v1/auth/login"
+                ).permitAll()
+
+                it.anyRequest().authenticated()
+            }
+
+            .httpBasic { it.disable() }
+
+            .formLogin { it.disable() }
+
+        return http.build()
     }
 }
